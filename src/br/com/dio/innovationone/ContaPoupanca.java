0ebe6ea11e0b0a5f8bb0d73a672d3f07013794a9
celiovjunior <celio_cjr@hotmail.com>
2022-07-17 20:37:26 -0300

    Insert the getters and setters in the classes.

--- src/br/com/dio/innovationone/ContaPoupanca.java
+++ src/br/com/dio/innovationone/ContaPoupanca.java
@@ -1,9 +1,9 @@
 package br.com.dio.innovationone;
 
 public class ContaPoupanca {
-    public int agencia;
-    public int numero;
-    public double saldo;
+    private int agencia;
+    private int numero;
+    private double saldo;
 
     public void sacar() {
 
